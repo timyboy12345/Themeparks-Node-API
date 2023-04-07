fd5304cf4286f1424c0f718a0685915bbfeb65bd
Tim Arendsen <t.j.m.arendsen@gmail.com>
2023-04-07 23:17:00 +0200

    v0.2.13.1: Fixed Walibi France test

--- src/parks/walibi/france/walibi-france.service.spec.ts
+++ src/parks/walibi/france/walibi-france.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WalibiFranceService } from './walibi-france.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { WalibiTransferService } from '../walibi-transfer/walibi-transfer.service';
 
 describe('WalibiFranceService', () => {
   let service: WalibiFranceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WalibiFranceService],
+      imports: [HttpModule, ConfigModule.forRoot()],
+      providers: [WalibiFranceService, WalibiTransferService],
     }).compile();
 
     service = module.get<WalibiFranceService>(WalibiFranceService);
@@ -14,5 +18,14 @@ describe('WalibiFranceService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return info', () => {
+    expect(service.getInfo().id).toBeDefined();
+  });
+
+  it('should return a list of POIs', async () => {
+    const data = await service.getPois();
+    expect(data).toBeInstanceOf(Array);
   });
 });
