840a45e4c9d525b55d04a707c125ccdd9d6f706f
Tim Arendsen <t.j.m.arendsen@gmail.com>
2023-04-18 20:32:38 +0200

    v0.2.15.1: Fixed Parques de Reunidos test

--- src/parks/parques-reunidos/parques-reunidos-park.service.spec.ts
+++ src/parks/parques-reunidos/parques-reunidos-park.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ParquesReunidosParkService } from './parques-reunidos-park.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { ParquesReunidosTransfer } from './parques-reunidos-transfer/parques-reunidos.transfer';
 
 describe('ParquesReunidosParkService', () => {
   let service: ParquesReunidosParkService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ParquesReunidosParkService],
+      providers: [ParquesReunidosParkService, ParquesReunidosTransfer],
+      imports: [HttpModule, ConfigModule.forRoot()],
     }).compile();
 
     service = module.get<ParquesReunidosParkService>(ParquesReunidosParkService);
